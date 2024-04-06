3bc691f0f096ecffcdf361d2bab99cd23d6f02b3
Haile <haile4cmd@gmail.com>
2024-04-06 13:11:38 +0300

    style: create the add service ui

--- client/src/pages/Admin/AddService/AddService.tsx
+++ client/src/pages/Admin/AddService/AddService.tsx
@@ -10,8 +10,38 @@ function AddService() {
                 <Logout/>
             </div>
         </div>
-        <div className="w-10/12 bg-white h-[50%] rounded-xl flex justify-start items-center flex-col shadow-md">
-            .
+        <div className="w-10/12 bg-white h-[45%] rounded-xl flex justify-start items-center flex-col shadow-md">
+            <div className="w-[95%] h-[15%] flex justify-start items-end">
+                <p className='text-primary-color text-lg font-semibold mb-5'>Ingresa la información del servicio para continuar</p>
+            </div>
+            <form className="w-[97%] flex justify-start flex-wrap">
+                <div className="m-3">
+                    <label htmlFor="countries" className="block mb-2 text-sm font-light text-gray-900 dark:text-white">Tipo de cliente</label>
+                    <select id="countries" className="bg-gray-50 border font-light border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 py-2.5 px-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <option value="new" className='bg-gray-50 border font-light border-gray-300 text-gray-900 text-sm rounded-lg '>Cliente nuevo</option>
+                        <option value="old" className='bg-gray-50 border font-light border-gray-300 text-gray-900 text-sm rounded-lg'>Cliente existente</option>
+                    </select>
+                </div>
+                <div className='m-3'>
+                    <label htmlFor="first_name" className="block mb-2 text-sm font-light text-gray-900 dark:text-white">Nombre del cliente</label>
+                    <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Nombre" required />
+                </div>
+                <div className='m-3'>
+                    <label htmlFor="last_name" className="block mb-2 text-sm font-light text-gray-900 dark:text-white">Nombre del servicio</label>
+                    <input type="text" id="last_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Servicio" required />
+                </div>
+                <div className='m-3'>
+                    <label htmlFor="last_name" className="block mb-2 text-sm font-light text-gray-900 dark:text-white">Email</label>
+                    <input type="text" id="last_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Email" required />
+                </div>
+            </form>
+            <div className="w-[95%] mt-10 flex justify-start items-center">
+                <input id="bordered-checkbox-1" type="checkbox" value="" name="bordered-checkbox" className="w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+                <label htmlFor="bordered-checkbox-1" className="w-full py-4 ms-2 text-sm font-semibold font-roboto text-gray-900 dark:text-gray-300">Recibir notificación por correo</label>
+            </div>
+        </div>
+        <div className="w-10/12 h-[10%] flex justify-end items-center ">
+            <button className='w-28 bg-primary-color text-white px-28 py-3 rounded-lg flex justify-center items-center'>Agregar</button>
         </div>
     </div>
   )
