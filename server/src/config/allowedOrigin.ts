2dc59f1646f8d83ca54eb7b21a555c7482435225
Haile-08 <haile4cmd@gmail.com>
2025-02-02 16:00:25 +0300

    feat: allow from www too

--- server/src/config/allowedOrigin.ts
+++ server/src/config/allowedOrigin.ts
@@ -1,6 +1,6 @@
 const allowedOrigins = {
-  origin: 'https://clientes.atend.mx',
+  origin: ['https://clientes.atend.mx', 'https://www.clientes.atend.mx'],
   credentials: true,
 };
-  
-export default allowedOrigins;  
+
+export default allowedOrigins;
